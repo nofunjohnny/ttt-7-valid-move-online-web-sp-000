c5c71ca187bf8c2666fa83b152b2fc0b40cb3b1e
John Zapata <john.zapata@thezapata.com>
2018-11-21 13:05:49 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,8 @@
 # code your #valid_move? method here
 def valid_move? (board, index)
-  if board[index] == nil || board[index] == 'X' || board[index] == '0' || index.between?(0, 8) == false
+  if  board[index] == nil || board[index] == 'X' || board[index] == 'O' || index.between?(0, 8) == false
     false
-  elsif board[index] == '' || board[index] == ''
+  elsif board[index] == ' ' || board[index] == ''
     true
   end
 end
